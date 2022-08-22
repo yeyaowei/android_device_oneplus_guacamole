20b6a1307d32b2e4633ba9e4b83ff60ff5d01faf
yeyaowei <yeyaowei@live.com>
2022-08-22 14:25:13 +0800

    guacamole: Remove MusicFX - Already implemented Dolby

--- device.mk
+++ device.mk
@@ -419,6 +419,10 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
